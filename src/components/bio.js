61f7202db101d97dc416e0aa1df1bb04b1a8526f
Chiiip <pedro_pianista@hotmail.com>
2020-01-05 16:02:24 -0300

    updating profile info

--- src/components/bio.js
+++ src/components/bio.js
@@ -34,8 +34,7 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Escrito por <strong>{author}</strong>, desenvolvedor soteropolitano e apaixonado por música.
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
                 You should follow him on Twitter
